dc1fd5d4eda6cd226521e988e528bade2e82d604
chinaza-Sommie <85754500+chinaza-Sommie@users.noreply.github.com>
2021-08-23 06:43:50 -0700

    images preview update

--- js/js-file.js
+++ js/js-file.js
@@ -36,22 +36,60 @@
 // 	openTabs(event, 'dashboard');
 // }
 
-// IMAGE PREVIEW =======================
+// ================== IMAGE PREVIEW =======================
 
-function filePreview(input){
-	if (input.files && input.files[0]) {
-		var reader = new fileReader();
-		reader.onload = function(e){
-			$('#uploadForm + img').remove();
-			$('#uploadForm').after('<img src="'+e.target.result+'" width="450" height="100"/>');
-		};
-		reader.readAsDataURL(input.files[0]);
-	}
-}
+var images = [];
+		function image_select(){
+			var image = document.getElementById('image').files;
+			for(i= 0; i < image.length; i++){
+				if (check_duplicate(image[i].name)){
+					images.push({
+						"name" : image[i].name,
+						"url" : URL.createObjectURL(image[i]),
+						"file" : image[i],
+					})
+				}else{
+					alert(image[i].name + "is already added to the list");
+				}
+			}
+			document.getElementById('form').reset();
+			document.getElementById('container').innerHTML = image_show();
+		}
 
-$("#file").change(function(){
-	filePreview(this);
-})
+		function image_show(){
+			var image = "";
+			images.forEach((i) => {
+				image += `<div class="col-lg-6 col mt-4">
+							<div class="product-order-body-img">
+								<div class="position-relative">
+									<img src="`+ i.url +`" class="edit-img">
+									<span class="position-absolute" onclick="delete_image(`+ images.indexOf(i) +`)">&times;</span>
+								</div>
+							</div>
+						</div>`;
+			})
+			return image;
+		}
+
+		function delete_image(e){
+			images.splice(e, 1);
+			document.getElementById('container').innerHTML = image_show();
+		}
+
+		function check_duplicate(name){
+			var image = true;
+			if(images.length > 0){
+				for (e = 0; e < images.length; e++){
+					if(images[e].name == name){
+						image = false;
+						break;
+					}
+				}
+			}
+
+			return image;
+		}
+
 
 
 // =========== page redirect
