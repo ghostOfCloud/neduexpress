0c03645229187b3ab3bff1b18d3ca1fb88c74307
chinaza-Sommie <85754500+chinaza-Sommie@users.noreply.github.com>
2021-08-26 22:46:30 -0700

    carousel update

--- vendor-dash/items-details.php
+++ vendor-dash/items-details.php
@@ -74,7 +74,32 @@
                             </div>
 
                             <div class="image-item animated zoomIn">
-                                <img src="../images/product_img/portfolio-5.jpg" class="img-fluid" style="width: 100%; height: 100%">
+                                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="width: 100%; height: 100%;">
+                                    <ol class="carousel-indicators">
+                                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                                    </ol>
+                                    <div class="carousel-inner" style="width: 100%; height: 100%;">
+                                        <div class="carousel-item active" style="width: 100%; height: 100%">
+                                          <img src="../images/product_img/portfolio-5.jpg" class="img-fluid" style="width:100%;height: 100%">
+                                        </div>
+                                        <div class="carousel-item" style="width: 100%; height: 100%;">
+                                          <img src="../images/product_img/portfolio-2.jpg" class="img-fluid" style="width:100%;height: 100%">
+                                        </div>
+                                        <div class="carousel-item" style="width: 100%; height: 100%;">
+                                          <img src="../images/product_img/portfolio-8.jpg" class="img-fluid" style="width:100%;height: 100%">
+                                        </div>
+                                    </div>
+                                      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                        <span class="sr-only">Previous</span>
+                                      </a>
+                                      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                        <span class="sr-only">Next</span>
+                                      </a>
+                                </div>
                             </div>
                         </div>
 
